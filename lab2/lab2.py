fe795946e7537b3177e8ebc91618144cac469b9a
rocketeerLi <31384630+rocketeerli@users.noreply.github.com>
2018-11-07 00:44:35 +0800

    版本1.2.-1 读取压缩文件 发现取对数问题... 待解决...

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -52,7 +52,35 @@ def calSignalToNoiseRatio(wave_data, decompressed_data) :
         sum_mum = sum_mum + sub * sub
     return 10 * math.log10(float(sum_son) / float(sum_mum))
 
-for i in range(10) :
+# 读取压缩文件
+def readCompressedFile(file_address) :
+    compressed_data = []          #用来存储压缩数据的数组
+    f = open(file_address, 'rb')
+    compressed_str = f.read()
+    # 取出第一个压缩数据，即第一个样本点
+    data_first = np.fromstring(compressed_str[0:16], dtype = np.short)
+    compressed_data.append(data_first)
+    # 去除第一个样本点，剩余所有数据都以 4 bit 存储
+    compressed_str = compressed_str[16:len(compressed_str)]
+    compressed_data_append = np.fromstring(compressed_str, dtype = np.uint8)
+    # 将读取出来的数据装进压缩数组中，每一个数据，拆成两个 4 bit 数
+    for num in compressed_data_append :
+        # 存储的时候，是转成 4 bit 无符号整数存储的， 解密时，需要转换回来
+        compressed_data.append((num >> 4) - 8)
+        compressed_data.append(((np.uint8(num << 4)) >> 4) - 8)
+    return compressed_data
+
+# 解密 还原文件
+def decompressWaveFile(compressed_data) :
+    decompressed_data = []
+    decompressed_data.append(compressed_data[0])
+    for i in range(len(compressed_data)) :
+        if i == 0 : 
+            continue
+        decompressed_data.append(decompressed_data[i - 1] + compressed_data[i] - 8)
+    return decompressed_data
+
+for i in range(1) :
     f = wave.open("./语料/" + str(i + 1) + ".wav","rb")
     # getparams() 一次性返回所有的WAV文件的格式信息
     params = f.getparams()
@@ -66,10 +94,17 @@ for i in range(10) :
     print( "采样点数目：" + str(len(wave_data)))          #输出应为采样点数目
     f.close()
     compressed_data, decompressed_data = compressWaveFile(wave_data)
+
     # 写压缩文件
     with open("./压缩文件/" + str(i + 1) + ".dpc", "wb") as f :
         for num in compressed_data :
             f.write(np.int16(num))
+    
+    # 读压缩文件 解压
+    with open("./压缩文件/" + str(i + 1) + ".dpc", "rb") as f :
+        compressed_data = readCompressedFile(f)
+        decompressed_data = decompressWaveFile(compressed_data)
+
     # 写还原文件
     with open("./还原文件/" + str(i + 1) + ".pcm", "wb") as f :
         for num in decompressed_data :
