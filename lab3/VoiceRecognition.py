e38bf6378672c681ffba98584ebea7aebf5f1bf1
rocketeerLi <31384630+rocketeerli@users.noreply.github.com>
2018-11-25 09:19:32 +0800

    版本1.1 DTW算法修正 & 更改代码结构

--- lab3/VoiceRecognition.py
+++ lab3/VoiceRecognition.py
@@ -4,6 +4,46 @@ import os
 import numpy as np
 from struct import unpack
 
+# 读取已经用 HTK 计算好的 MFCC 特征
+def getMFCC() :
+    MFCC = []
+    for i in range(10) :
+        MFCC_rows = []
+        for j in range(5) :
+            f = open("./RecordedVoice-MFCC/" + str(i + 1) + "-" + str(j + 1) + ".mfc","rb")
+            nframes = unpack(">i", f.read(4))[0]
+            frate = unpack(">i", f.read(4))[0]     # 100 ns 内的
+            nbytes = unpack(">h", f.read(2))[0]    # 特征的字节数
+            feakind = unpack(">h", f.read(2))[0]
+            # print("nframes : " + str(nframes) + "\n" + "frate : " + str(frate) + "\n" + \
+            #         "nbytes : " + str(nbytes) + "\n" + "feakind : " + str(feakind))
+            ndim = nbytes / 4   # 维数
+            feature = []
+            for m in range(nframes) :
+                feature_frame = []
+                for n in range(int(ndim)) :
+                    feature_frame.append(unpack(">f", f.read(4))[0])
+                feature.append(feature_frame)
+            f.close()
+            MFCC_rows.append(feature)
+        MFCC.append(MFCC_rows)
+    return MFCC
+
+# 取出其中的模板命令的 MFCC 特征 
+def getMFCCModels(MFCC) :
+    MFCC_models = []
+    for i in range(len(MFCC)) :
+        MFCC_models.append(MFCC[i][0])
+    return MFCC_models
+
+# 取出其中的待分类语音的 MFCC 特征
+def getMFCCUndetermined(MFCC) :
+    MFCC_undetermined = []
+    for i in range(len(MFCC)) :
+        for j in range(1, len(MFCC[i])) :
+            MFCC_undetermined.append(MFCC[i][j])
+    return MFCC_undetermined
+
 # DTW 算法...
 def dtw(M1, M2) :
     # 初始化数组 大小为 M1 * M2
@@ -22,9 +62,9 @@ def dtw(M1, M2) :
     # 初始化 cost 的第 0 行和第 0 列
     cost[0][0] = dis[0][0]
     for i in range(1, M1_len) :
-        cost[i][0] = cost[i - 1][0]
+        cost[i][0] = cost[i - 1][0] + dis[i][0]
     for j in range(1, M2_len) :
-        cost[0][j] = cost[0][j - 1]
+        cost[0][j] = cost[0][j - 1] + dis[0][j]
     
     # 开始动态规划
     for i in range(1, M1_len) :
@@ -42,40 +82,14 @@ def distance(x1, x2) :
     return sum
 
 # 存储所有语音文件的 MFCC 特征
-MFCC = []
-
 # 读取已经用 HTK 计算好的 MFCC 特征
-for i in range(10) :
-    MFCC_rows = []
-    for j in range(5) :
-        f = open("./RecordedVoice-MFCC/" + str(i + 1) + "-" + str(j + 1) + ".mfc","rb")
-        nframes = unpack(">i", f.read(4))[0]
-        frate = unpack(">i", f.read(4))[0]     # 100 ns 内的
-        nbytes = unpack(">h", f.read(2))[0]    # 特征的字节数
-        feakind = unpack(">h", f.read(2))[0]
-        # print("nframes : " + str(nframes) + "\n" + "frate : " + str(frate) + "\n" + \
-        #         "nbytes : " + str(nbytes) + "\n" + "feakind : " + str(feakind))
-        ndim = nbytes / 4   # 维数
-        feature = []
-        for m in range(nframes) :
-            feature_frame = []
-            for n in range(int(ndim)) :
-                feature_frame.append(unpack(">f", f.read(4))[0])
-            feature.append(feature_frame)
-        f.close()
-        MFCC_rows.append(feature)
-    MFCC.append(MFCC_rows)
+MFCC = getMFCC()
 
 # 取出其中的模板命令的 MFCC 特征 
-MFCC_models = []
-for i in range(len(MFCC)) :
-    MFCC_models.append(MFCC[i][0])
+MFCC_models = getMFCCModels(MFCC)
 
 # 取出其中的待分类语音的 MFCC 特征
-MFCC_undetermined = []
-for i in range(len(MFCC)) :
-    for j in range(1, len(MFCC[i])) :
-        MFCC_undetermined.append(MFCC[i][j])
+MFCC_undetermined = getMFCCUndetermined(MFCC)
 
 # 开始匹配
 for i in range(len(MFCC_undetermined)) :
