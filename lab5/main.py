13bcf997de8ab46567ac76991eb9dc407fcbbb6b
rocketeerLi <31384630+rocketeerli@users.noreply.github.com>
2018-12-13 22:47:43 +0800

    均值滤波&中值滤波 version1.0

--- lab5/main.py
+++ lab5/main.py
@@ -4,29 +4,41 @@ import cv2
 import imageAdjust
 import matplotlib.pyplot as plt
 import imageHistogram
+import ordinaryFilter
 
-
+# 计算二维矩阵的中值
+def calMedianValue(matrix) :
+    return np.median(matrix)
 
 # 读取图片信息
-img = cv2.imread("xunmeng.bmp")
+img = cv2.imread("02.jpg")
+cv2.imshow("original", img)
 # # 颜色空间转换 BGR -> HLS
 # hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
 # cv2.imshow("HLS",hls)  
 # 通道分离
 # h, l, s = cv2.split(hls)
 
-# 第一部分 实现对图像的亮度、对比度、饱和度、色度的调整。（
-cv2.imshow("origin image", img)
-# 调整图像亮度
-imageAdjust.imageAdjustmentLightness(img)
-# 调整图像对比度
-imageAdjust.imageAdjustmentContrast(img)
-# 调整图像饱和度
-imageAdjust.imageAdjustmentSaturation(img)
-# 调整图像色度
-imageAdjust.imageAdjustmentHue(img)
+# # 第一部分 实现对图像的亮度、对比度、饱和度、色度的调整。（
+# cv2.imshow("origin image", img)
+# # 调整图像亮度
+# imageAdjust.imageAdjustmentLightness(img)
+# # 调整图像对比度
+# imageAdjust.imageAdjustmentContrast(img)
+# # 调整图像饱和度
+# imageAdjust.imageAdjustmentSaturation(img)
+# # 调整图像色度
+# imageAdjust.imageAdjustmentHue(img)
 
-# 第二部分 统计图像的直方图。
-imageHistogram.drawHistogram(img)
+# # 第二部分 统计图像的直方图。
+# imageHistogram.drawHistogram(img)
+
+# 第三部分 实现图像的空域滤波：中值滤波和均值滤波
+# 中值滤波
+img_median = ordinaryFilter.medianFiltering(img)
+cv2.imshow("median filtering", img_median)
+# 均值滤波
+img_mean = ordinaryFilter.meanFiltering(img)
+cv2.imshow("mean filtering", img_mean)
 
 cv2.waitKey(0)
---
+++ lab5/ordinaryFilter.py
@@ -0,0 +1,79 @@
+# -*- coding: UTF-8 -*-
+import numpy as np
+import cv2
+
+# 十字 窗口，中值滤波
+def medianFiltering(img) :
+    B, G, R = cv2.split(img)
+    # 对 蓝色通道 进行均值滤波
+    for row in range(len(B) - 1) :
+        for col in range(len(B[row]) - 1) :
+            # 边界，暂时不处理
+            if row == 0 or col == 0 :
+                continue
+            # 3 * 3
+            # arr = B[[row - 1, row, row + 1], :]
+            # arr = arr[:, [col - 1, col, col + 1]]
+            # 十字
+            arr = np.array([B[row - 1][col], B[row][col - 1], B[row][col], B[row][col + 1], B[row + 1][col]])
+            B[row][col] = np.int8(np.median(arr))
+    # 对 绿色通道 进行均值滤波
+    for row in range(len(G) - 1) :
+        for col in range(len(G[row]) - 1) :
+            # 边界，暂时不处理
+            if row == 0 or col == 0 :
+                continue
+            # 3 * 3
+            # arr = G[[row - 1, row, row + 1], :]
+            # arr = arr[:, [col - 1, col, col + 1]]
+            # 十字
+            arr = np.array([G[row - 1][col], G[row][col - 1], G[row][col], G[row][col + 1], G[row + 1][col]])
+            G[row][col] = np.int8(np.median(arr))
+    # 对 红色通道 进行均值滤波
+    for row in range(len(R) - 1) :
+        for col in range(len(R[row]) - 1) :
+            # 边界，暂时不处理
+            if row == 0 or col == 0 :
+                continue
+            # 3 * 3
+            # arr = R[[row - 1, row, row + 1], :]
+            # arr = arr[:, [col - 1, col, col + 1]]
+            # 十字
+            arr = np.array([R[row - 1][col], R[row][col - 1], R[row][col], R[row][col + 1], R[row + 1][col]])
+            R[row][col] = np.int8(np.median(arr))
+    return cv2.merge([B, G, R])
+
+# 十字 窗口，均值滤波
+def meanFiltering(img) :
+    B, G, R = cv2.split(img)
+    # 对 蓝色通道 进行均值滤波
+    for row in range(len(B) - 1) :
+        for col in range(len(B[row]) - 1) :
+            # 边界，暂时不处理
+            if row == 0 or col == 0 :
+                continue
+            # 十字
+            arr = np.array([B[row - 1][col], B[row][col - 1], B[row][col], B[row][col], B[row][col], \
+                            B[row][col + 1], B[row + 1][col]])
+            B[row][col] = np.int8(np.mean(arr))
+    # 对 绿色通道 进行均值滤波
+    for row in range(len(G) - 1) :
+        for col in range(len(G[row]) - 1) :
+            # 边界，暂时不处理
+            if row == 0 or col == 0 :
+                continue
+            # 十字
+            arr = np.array([G[row - 1][col], G[row][col - 1], G[row][col], G[row][col], G[row][col], \
+                            G[row][col + 1], G[row + 1][col]])
+            G[row][col] = np.int8(np.mean(arr))
+    # 对 红色通道 进行均值滤波
+    for row in range(len(R) - 1) :
+        for col in range(len(R[row]) - 1) :
+            # 边界，暂时不处理
+            if row == 0 or col == 0 :
+                continue
+            # 十字
+            arr = np.array([R[row - 1][col], R[row][col - 1], R[row][col], R[row][col], R[row][col], \
+                            R[row][col + 1], R[row + 1][col]])
+            R[row][col] = np.int8(np.mean(arr))
+    return cv2.merge([B, G, R])
