3639bc54e9e3a4f493e2a002f3583816be61c028
rocketeerLi <31384630+rocketeerli@users.noreply.github.com>
2018-12-25 11:31:04 +0800

    快速双边滤波

--- lab6/main.py
+++ lab6/main.py
@@ -3,52 +3,88 @@ import numpy as np
 import cv2
 import math
 
-def bilateralFilter(img, rows, cols, sigmaColor, sigmaSpace) : 
+def bilateralFilter(img, radius, sigmaColor, sigmaSpace) : 
     B, G, R = cv2.split(img)
     B_tran, G_tran, R_tran = cv2.split(img)
-    cv2.imshow("Bg", B)
     img_height = len(B)
     img_width = len(B[0])
-    dis_space = math.exp(-((i-k)**2 + (j-l)**2)/(2*sigmaSpace*sigmaSpace))
-    for i in range(img_height) :
-        for j in range(img_width) :
+    # 计算灰度值模板系数表
+    color_coeff = -0.5 / (sigmaColor * sigmaColor)
+    weight_color = []       # 存放颜色差值的平方
+    for i in range(256) :
+        weight_color.append(np.exp(i * i * color_coeff))
+    # 计算空间模板
+    space_coeff = -0.5 / (sigmaSpace * sigmaSpace)
+    weight_space = []     # 存放模板系数
+    weight_space_row = [] # 存放模板 x轴 位置
+    weight_space_col = [] # 存放模板 y轴 位置
+    maxk = 0
+    for i in range(-radius, radius+1) :
+        for j in range(-radius, radius+1) :
+            r_square = i*i + j*j
+            r = np.sqrt(r_square)
+            weight_space.append(np.exp(r_square * space_coeff))
+            weight_space_row.append(i)
+            weight_space_col.append(j)
+            maxk = maxk + 1
+    # 进行滤波
+    for row in range(img_height) :
+        for col in range(img_width) :
             value = 0
             weight = 0
-            # 计算一个 rows * cols 的矩阵
-            # rows * cols 的矩阵
-            for k in range(i - int((rows-1)/2), i + int((rows+1)/2)) :
-                for l in range(j - int((cols-1)/2), j + int((cols+1)/2)) :
-                    # 计算 定义域核 dis_space 
-                    # 计算颜色值
-                    if k < 0 or k > img_height-1 or l < 0 or l > img_width-1 :
-                        f_kl = 0
-                    else :
-                        f_kl = B[k][l]
-                    # 计算 值域核 dis_color
-                    dis_color = math.exp(-((int(B[i][j]) - int(f_kl))**2)/(2*sigmaColor*sigmaColor))
-                    # 权值
-                    weight_value = dis_space * dis_color
-                    # 计算加权值
-                    # print("k:" + str(k) + "\tl:" + str(l) + "\tvalue : " + str(value) + "\tf_kl : " + str(f_kl))
-                    value = value + f_kl * weight_value
-                    # 计算权值和
-                    weight = weight + weight_value
-            # print("value : " + str(value) + "\tweight : " + str(weight))
-            B_tran[i][j] = value / weight
-    cv2.imshow("BGR", B_tran)
+            for i in range(maxk) :
+                m = row + weight_space_row[i]
+                n = col + weight_space_col[i]
+                if m < 0 or n < 0 or m >= img_height or n >= img_width :
+                    val = 0
+                else :
+                    val = B[m][n]
+                w = np.float32(weight_space[i]) * np.float32(weight_color[np.abs(val - B[row][col])])
+                value = value + val * w
+                weight = weight + w
+            B_tran[row][col] = np.uint8(value / weight)
+    # 绿色通道
+    for row in range(img_height) :
+        for col in range(img_width) :
+            value = 0
+            weight = 0
+            for i in range(maxk) :
+                m = row + weight_space_row[i]
+                n = col + weight_space_col[i]
+                if m < 0 or n < 0 or m >= img_height or n >= img_width :
+                    val = 0
+                else :
+                    val = G[m][n]
+                w = np.float32(weight_space[i]) * np.float32(weight_color[np.abs(val - G[row][col])])
+                value = value + val * w
+                weight = weight + w
+            G_tran[row][col] = np.uint8(value / weight)
+    # 红色通道
+    for row in range(img_height) :
+        for col in range(img_width) :
+            value = 0
+            weight = 0
+            for i in range(maxk) :
+                m = row + weight_space_row[i]
+                n = col + weight_space_col[i]
+                if m < 0 or n < 0 or m >= img_height or n >= img_width :
+                    val = 0
+                else :
+                    val = R[m][n]
+                w = np.float32(weight_space[i]) * np.float32(weight_color[np.abs(val - R[row][col])])
+                value = value + val * w
+                weight = weight + w
+            R_tran[row][col] = np.uint8(value / weight)
+    cv2.imshow("beauty_after", cv2.merge([B_tran, G_tran, R_tran]))
     cv2.imwrite("beauty_after.png", cv2.merge([B_tran, G_tran, R_tran]))
 
-img = cv2.imread("beauty.png")
+img = cv2.imread("beauty1.png")
 cv2.imshow("original image", img)
 
-dst = cv2.bilateralFilter(src=img, d=0, sigmaColor=100, sigmaSpace=15)
-cv2.namedWindow('bi_demo',0)
-cv2.resizeWindow('bi_demo',300,400)
-cv2.imshow("bi_demo", dst)
+# bilateralFilter(img, 5, 45, 100)
+bilateralFilter(img, 3, 30, 80)
 
-bilateralFilter(img, 27, 27, 45, 100)
+img = cv2.imread("beauty_after.png")
+bilateralFilter(img, 3, 30, 80)
 
-cv2.namedWindow('show',0)
-cv2.resizeWindow('show',300,400)
-cv2.imshow("show", img)
 cv2.waitKey(0)
